be647c783689cf56cad5406f575dfbe97dac8061
Hwanu Jung <sossont1@gmail.com>
2022-08-31 21:51:08 +0900

    [22.08.31] React 입문 1~9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import InputSample from "./InputSample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = 'Example';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24,
+        padding: '1rem'
+    }
+
+    return (
+        <>
+            <InputSample/>
+        </>
+    );
 }
 
 export default App;
